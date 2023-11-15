05f3012bc74c8a26ed361539a8c16f7a1b29cae1
Mohit Aggarwal <mohitaggarwal@Mohits-MacBook-Pro.local>
2023-11-15 16:55:11 +0530

    tested login with email, password from registered cognito user

--- src/Mqtt311.tsx
+++ src/Mqtt311.tsx
@@ -1,9 +1,13 @@
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { mqtt, iot, CrtError, auth } from "aws-iot-device-sdk-v2";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
-import { CognitoIdentityClient, GetIdCommand } from "@aws-sdk/client-cognito-identity";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import {
+    AuthenticationDetails,
+    CognitoUser,
+    CognitoUserPool,
+} from 'amazon-cognito-identity-js';
 import { AWS_REGION, AWS_COGNITO_IDENTITY_POOL_ID, AWS_IOT_ENDPOINT } from './settings';
-import jquery from "jquery";
 import TableTwo from './components/TableTwo.tsx';
 import DeviceMap from './components/DeviceMap.jsx';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,10 +15,9 @@ import { updateDeviceState } from './redux/actions/deviceActions';
 import { updateSeriesShadow } from './redux/actions/seriesActions';
 import extractData from './js/extractData';
 
-const $: JQueryStatic = jquery;
-
 function log(msg: string) {
-    $('#message').append(`<pre>${msg}</pre>`);
+    // Assuming you have an element with id 'message' to log messages
+    document.getElementById('message').innerHTML += `<pre>${msg}</pre>`;
 }
 
 interface AWSCognitoCredentialOptions {
@@ -42,13 +45,14 @@ export class AWSCognitoCredentialsProvider extends auth.CredentialsProvider {
     }
 
     getCredentials() {
+        console.log(this.aws_credentials);
         return this.aws_credentials;
     }
 
     async refreshCredentialAsync() {
         try {
             const credentials = await fromCognitoIdentityPool({
-                client: new CognitoIdentityClient({ region: this.options.Region }), // Add the region option here
+                client: new CognitoIdentityClient({ region: this.options.Region }),
                 identityPoolId: this.options.IdentityPoolId,
             })();
 
@@ -107,9 +111,143 @@ async function connect_websocket(provider: auth.CredentialsProvider) {
     });
 }
 
+function Login({ onLogin }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [isPasswordChangeRequired, setIsPasswordChangeRequired] = useState(false);
+
+    const handleLogin = async () => {
+        const poolData = {
+            UserPoolId: 'us-east-1_vBr9qRmmd',
+            ClientId: '5dngq7m5g8qsi91ephbp1lovn0',
+        };
+        const userPool = new CognitoUserPool(poolData);
+
+        const authenticationData = {
+            Username: email,
+            Password: password,
+        };
+        const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+        const userData = {
+            Username: email,
+            Pool: userPool,
+        };
+        const cognitoUser = new CognitoUser(userData);
+
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: (session) => {
+                console.log('Authentication successful:', session);
+                onLogin();
+            },
+            onFailure: (err) => {
+                console.error('Authentication failed:', err);
+            },
+            newPasswordRequired: (userAttributes, requiredAttributes) => {
+                console.log('New password required:', userAttributes, requiredAttributes);
+                setIsPasswordChangeRequired(true);
+            },
+        });
+    };
+
+    const handleNewPasswordChange = async () => {
+        const poolData = {
+            UserPoolId: 'us-east-1_vBr9qRmmd',
+            ClientId: '5dngq7m5g8qsi91ephbp1lovn0',
+        };
+        const userPool = new CognitoUserPool(poolData);
+    
+        console.log(userPool);
+        const userData = {
+            Username: email,
+            Pool: userPool,
+        };
+        
+        console.log(userData);
+        const authenticationData = {
+            Username: email,
+            Password: password,
+        };
+        console.log(authenticationData);
+        
+        const authenticationDetails = new AuthenticationDetails(authenticationData);
+        console.log(authenticationDetails);
+        const cognitoUser = new CognitoUser(userData);
+        console.log(cognitoUser);
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: (session) => {
+                console.log(session)
+                cognitoUser.completeNewPasswordChallenge(newPassword, {}, {
+                    onSuccess: (session) => {
+                        console.log('New password set successfully:', session);
+                        // After setting the new password, you can redirect the user to the main application
+                        onLogin();
+                    },
+                    onFailure: (err) => {
+                        console.error('Failed to set new password:', err);
+                        // Handle the failure scenario, e.g., show an error message to the user
+                    },
+                });
+            },
+            onFailure: (err) => {
+                console.error('Authentication failed:', err);
+                // Handle the failure scenario, e.g., show an error message to the user
+            },
+            newPasswordRequired: (userAttributes, requiredAttributes) => {
+                console.log(userAttributes, requiredAttributes);
+                        // Continue with setting the new password
+                cognitoUser.completeNewPasswordChallenge(newPassword, {}, {
+                    onSuccess: (session) => {
+                        console.log('New password set successfully:', session);
+                        // After setting the new password, you can redirect the user to the main application
+                        onLogin();
+                    },
+                    onFailure: (err) => {
+                        console.error('Failed to set new password:', err);
+                        // Handle the failure scenario, e.g., show an error message to the user
+                    },
+                });
+                // Additional logic may be needed here if additional attributes are required
+                // For example, you might prompt the user to enter additional information.
+            },
+        });
+    };
+    
+    
+
+    return (
+        <div>
+            <h2>{isPasswordChangeRequired ? 'Set New Password' : 'Login'}</h2>
+            {isPasswordChangeRequired ? (
+                <>
+                    <label>New Password:</label>
+                    <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+                    <br />
+                    <button onClick={handleNewPasswordChange}>Set New Password</button>
+                </>
+            ) : (
+                <>
+                    <label>Email:</label>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <br />
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <br />
+                    <button onClick={handleLogin}>Login</button>
+                </>
+            )}
+        </div>
+    );
+}
+
+// ... (rest of the code)
+
+
 function Mqtt311(arg) {
     const dispatch = useDispatch();
     let seriesData = useSelector((state) => state.series.seriesData);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     var connectionPromise: Promise<mqtt.MqttClientConnection>;
     var sample_msg_count = 0;
@@ -158,8 +296,14 @@ function Mqtt311(arg) {
     }
 
     useEffect(() => {
-        PubSub(); // initial execution
-    }, []);
+        if (isLoggedIn) {
+            PubSub(); // initial execution
+        }
+    }, [isLoggedIn]);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
 
     async function PublishMessage() {
         const msg = `BUTTON CLICKED {${user_msg_count}}`;
@@ -182,10 +326,15 @@ function Mqtt311(arg) {
     }
 
     return (
-        <>
-            {/*<TableTwo deviceState={deviceState} />*/}
-            {/*<DeviceMap deviceState={deviceState} />*/}
-        </>
+        <div>
+            {isLoggedIn ? (
+                <>
+                    {/* Render components when the user is logged in */}
+                </>
+            ) : (
+                <Login onLogin={handleLogin} />
+            )}
+        </div>
     );
 }
 
